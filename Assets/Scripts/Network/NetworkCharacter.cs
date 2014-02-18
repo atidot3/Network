f1c6ae78fd16f94e67f51d89a7e1d6fabacbee87
Oxym <atidote63240@gmail.com>
2014-02-18 02:52:10 +0100

    Init
    Networking setup

---
+++ Assets/Scripts/Network/NetworkCharacter.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkCharacter :  Photon.MonoBehaviour
+{
+	Vector3 realPosition = Vector3.zero;
+	Quaternion realRotation = Quaternion.identity;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (photonView.isMine)
+		{
+		} 
+		else
+		{
+			transform.position = Vector3.Lerp(transform.position, realPosition, 0.1f);
+			transform.rotation = Quaternion.Lerp(transform.rotation, realRotation, 0.1f);
+		}
+	}
+	public void OnPhotonSerializeView(PhotonStream steam, PhotonMessageInfo info)
+	{
+		if (steam.isWriting)
+		{
+			steam.SendNext(transform.position);
+			steam.SendNext(transform.rotation);
+		}
+		else
+		{
+			realPosition = (Vector3)steam.ReceiveNext();
+			realRotation = (Quaternion)steam.ReceiveNext();
+		}
+	}
+}
---
+++ Assets/Scripts/Network/NetworkManager.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkManager : MonoBehaviour {
+
+	// Use this for initialization
+
+	public GameObject standByCamera;
+	SpawnSpot[] spot;
+	void Start ()
+	{
+		spot = GameObject.FindObjectsOfType<SpawnSpot>();
+		Connect();
+	}
+	void Connect()
+	{
+		PhotonNetwork.ConnectUsingSettings("1.0.0");
+	}
+	void OnGUI()
+	{
+		GUILayout.Label (PhotonNetwork.connectionStateDetailed.ToString ());
+	}
+	void OnJoinedLobby()
+	{
+		PhotonNetwork.JoinRandomRoom ();
+		Debug.Log ("JoinRandomRoom");
+	}
+	void OnPhotonRandomJoinFailed()
+	{
+		PhotonNetwork.CreateRoom (null);
+		Debug.Log ("OnPhotonRandomJoinFailed");
+	}
+	void OnJoinedRoom()
+	{
+		Debug.Log ("OnJoinedRoom");
+		SpawnMyPlayer ();
+	}
+	void SpawnMyPlayer()
+	{
+		SpawnSpot mySpawnSpots = spot[Random.Range(0, spot.Length)];
+		GameObject myPlayerGo = (GameObject)PhotonNetwork.Instantiate("PlayerController", mySpawnSpots.transform.position, mySpawnSpots.transform.rotation, 0);
+		standByCamera.gameObject.SetActive(true);
+		((MonoBehaviour)myPlayerGo.GetComponent ("NetworkCharacter")).enabled = true;
+		((MonoBehaviour)myPlayerGo.GetComponent ("FPSInputController")).enabled = true;
+		((MonoBehaviour)myPlayerGo.GetComponent ("MouseLook")).enabled = true;
+		((MonoBehaviour)myPlayerGo.GetComponent ("CharacterMotor")).enabled = true;
+		myPlayerGo.transform.FindChild ("Main Camera").gameObject.SetActive(true);
+	}
+}
